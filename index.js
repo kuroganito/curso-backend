c619a135a87817ffdf0d4279de855cba4c5ad5a4
Oscar Alberto Paredes Garduno <oscar.paredes@infotec.mx>
2017-01-31 11:24:58 -0600

    Server

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -70,10 +72,10 @@ app.get('/', function (req, res) {
             sinopsis: "Basada en la línea de juguetes de Hasbro, esta adaptación ha sido descrita como un cruce entre Los Goonies y Transformers."
         }
         ]);
-    }, 3000);
+    }, 1000);
 
 });
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+app.listen(app.get('port'), function () {
+    console.log('Example app listening on port '+app.get('port'));
 });
